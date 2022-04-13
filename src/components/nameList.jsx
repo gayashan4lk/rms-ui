511cc34f4780f69f951ba9b903acc5916ee2d18e
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-04-13 23:54:45 +0530

    fetch data from api

--- src/components/nameList.jsx
+++ src/components/nameList.jsx
@@ -1,11 +1,34 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const API_ENDPOINT = "sdfs";
+
 
 const NameList = (props) => {
-    const [names, setNames] = useState([
-        { name : "Pepper Potts", id : 1 },
-        { name : "Micheal jones", id : 2 },
-        { name : "maria sisily", id : 3 }
-    ]);
+    // const [names, setNames] = useState([
+    //     { name : "Pepper Potts", id : 1 },
+    //     { name : "Micheal jones", id : 2 },
+    //     { name : "maria sisily", id : 3 }
+    // ]);
+
+    const [names, setNames] = useState([]);
+
+    const fetchData = React.useCallback(() => {
+        axios({
+            "method":"GET",
+            "url":"http://localhost:5099/api/candidates/get"
+        })
+        .then((response) => {
+            setNames(response.data)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    },[]);
+
+    useEffect(() => {
+        fetchData()
+    },[fetchData]);
 
 
     return(
@@ -16,7 +39,7 @@ const NameList = (props) => {
                 </a>
 
                 {names.map(name => {
-                    return (<a href="#" key={name.id} className="list-group-item list-group-item-action">{name.name}</a>);
+                    return (<a href="#" key={name.id} className="list-group-item list-group-item-action">{name.name} {name.phoneNumber} {name.comment}</a>);
                 })}
             </div>
         </div>
