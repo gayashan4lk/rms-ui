107a089d138f7dcc0eed5aad1c56f82cae5494be
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-04-17 21:33:05 +0530

    add react-route-dom v6

--- src/App.js
+++ src/App.js
@@ -4,38 +4,57 @@ import NavBar from './components/navbar';
 import NameList from './components/nameList';
 import UserForm from './components/userForm';
 import SideBar from "./components/sideBar";
+import {BrowserRouter, Routes, Route, Link, Outlet} from "react-router-dom";
+import Expenses from "./routes/expenses";
+import Invoices from "./routes/invoices";
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <div className="wrapper">
-        <SideBar />
-        <nav id="content">
-          {/* <div id="content">
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-              <div className="container-fluid">
-                <button id="sidebarCollapse" className="btn btn-info">
-                  <i className="fas fa-align-left"></i>
-                  <span>Toggle Sidebar</span>
-                </button>
-              </div>
-            </nav>
-          </div> */}
-          <NavBar data="This is data" />
-          <div className="container">
-            <div className="row">
-              <div className="col">
-              <NameList name="Tony Stark" />
-              </div>
-              <div className="col">
-              <UserForm />
-              </div>
-            </div>
-          </div>
-        </nav>
-      </div>
-    </div>
+    <React.Fragment>
+      <h1>hello</h1>
+      <Link to="/invoices">Invoices</Link>
+      <Link to="/expenses">Expenses</Link>
+      <Outlet />
+    </React.Fragment>
   );
 }
-
 export default App;
+
+
+      {/* <div className="App">
+        <div className="wrapper">
+          <SideBar />
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/dashboard">Dashboard</Link>
+          <Switch>
+            <Route path="/">
+
+            </Route>
+          </Switch>
+          <nav id="content">
+            <div id="content">
+              <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                  <button id="sidebarCollapse" className="btn btn-info">
+                    <i className="fas fa-align-left"></i>
+                    <span>Toggle Sidebar</span>
+                  </button>
+                </div>
+              </nav>
+            </div>
+            <NavBar data="This is data" />
+            <div className="container">
+              <div className="row">
+                <div className="col">
+                <NameList name="Tony Stark" />
+                </div>
+                <div className="col">
+                <UserForm />
+                </div>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </div> */}
---
+++ src/routes/invoices.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Invoices = () => {
+    return ( 
+        <main>
+            <h2>Invoices</h2>
+        </main>
+     );
+}
+ 
+export default Invoices;
