2c230325a00f62ecfeada49534f862a8780e8744
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-05-01 21:28:52 +0530

    move router from index to App

--- src/App.js
+++ src/App.js
@@ -12,49 +12,15 @@ import React from "react";
 function App() {
   return (
     <React.Fragment>
-      <h1>hello</h1>
-      <Link to="/invoices">Invoices</Link>
-      <Link to="/expenses">Expenses</Link>
-      <Outlet />
+      <BrowserRouter>
+       <NavBar />
+       <Routes >
+         <Route path="/namelist" element={<NameList/>} />
+         <Route path="/userform" element={<UserForm/>} />
+       </Routes>
+      </BrowserRouter>
+      
     </React.Fragment>
   );
 }
 export default App;
-
-
-      {/* <div className="App">
-        <div className="wrapper">
-          <SideBar />
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/dashboard">Dashboard</Link>
-          <Switch>
-            <Route path="/">
-
-            </Route>
-          </Switch>
-          <nav id="content">
-            <div id="content">
-              <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <div className="container-fluid">
-                  <button id="sidebarCollapse" className="btn btn-info">
-                    <i className="fas fa-align-left"></i>
-                    <span>Toggle Sidebar</span>
-                  </button>
-                </div>
-              </nav>
-            </div>
-            <NavBar data="This is data" />
-            <div className="container">
-              <div className="row">
-                <div className="col">
-                <NameList name="Tony Stark" />
-                </div>
-                <div className="col">
-                <UserForm />
-                </div>
-              </div>
-            </div>
-          </nav>
-        </div>
-      </div> */}
--- src/index.js
+++ src/index.js
@@ -10,14 +10,7 @@ import Invoices from "./routes/invoices";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} >
-        <Route path="expenses" element={<Expenses />} />
-        <Route path="invoices" element={<Invoices />} />
-      </Route>
-    </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
 
