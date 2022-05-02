065c95a6089a6c5c09a3a6198e6ccd94aeff107f
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-05-02 14:16:42 +0530

    install husky

--- src/components/userForm.jsx
+++ src/components/userForm.jsx
@@ -22,48 +22,7 @@ function UserForm() {
     console.log(errors);
   };
 
-  return (
-    <form onSubmit={handleSubmit(onFormSubmit, onErrors)} method="post">
-      <div className="form-group">
-        <label>
-          User Name
-          <input
-            type="text"
-            name="name"
-            {...register('name')}
-            id="name"
-            className="form-control"
-            placeholder="Enter your name"
-          />
-        </label>
-      </div>
-      <div className="form-group">
-        <label htmlFor="phoneNumber">Mobile Number</label>
-        <input
-          type="text"
-          name="phoneNumber"
-          {...register('phoneNumber')}
-          id="phoneNumber"
-          className="form-control"
-          placeholder="Enter your mobile number"
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="comment">Comments</label>
-        <textarea
-          name="comment"
-          {...register('comment')}
-          id="comment"
-          className="form-control"
-          cols="30"
-          rows="6"
-        />
-      </div>
-      <button type="submit" className="btn btn-primary" value="submit">
-        Submit
-      </button>
-    </form>
-  );
+  return <h1>sdad</h1>;
 }
 
 export default UserForm;
