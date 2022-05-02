f7acbf81e539cd6dea4857fed95f1f9e7e89b3d0
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-05-02 14:20:53 +0530

    test commit after husky integration

--- src/components/userForm.jsx
+++ src/components/userForm.jsx
@@ -22,7 +22,45 @@ function UserForm() {
     console.log(errors);
   };
 
-  return <h1>sdad</h1>;
+  return (
+    <form onSubmit={handleSubmit(onFormSubmit, onErrors)} method="post">
+      <div className="form-group">
+        <label htmlFor="name">User Name</label>
+        <input
+          type="text"
+          name="name"
+          {...register('name')}
+          className="form-control"
+          placeholder="Enter your name"
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="phoneNumber">Mobile Number</label>
+        <input
+          type="text"
+          name="phoneNumber"
+          {...register('phoneNumber')}
+          id="phoneNumber"
+          className="form-control"
+          placeholder="Enter your mobile number"
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="comment">Comments</label>
+        <textarea
+          name="comment"
+          {...register('comment')}
+          id="comment"
+          className="form-control"
+          cols="30"
+          rows="6"
+        />
+      </div>
+      <button type="submit" className="btn btn-primary" value="submit">
+        Submit
+      </button>
+    </form>
+  );
 }
 
 export default UserForm;
